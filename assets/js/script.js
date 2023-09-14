5f062772477477012281cd208e678fbfca4c68f2
kenabey <kebab.bahadur@gmail.com>
2023-09-14 12:51:56 +0545

    Added files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
-    console.log(entry);
     if (entry.isIntersecting) {
       entry.target.classList.add("show");
     } else {
@@ -10,3 +9,37 @@ const observer = new IntersectionObserver((entries) => {
 });
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
+
+// TO-THE-TOP BUTTON
+const topBtn = document.querySelector(".top-btn");
+
+const section = document.querySelector(".landing-page");
+const observer2 = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (ent.isIntersecting == false) {
+      topBtn.classList.remove("hide");
+    }
+    if (ent.isIntersecting == true) {
+      topBtn.classList.add("hide");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-50%",
+  }
+);
+observer2.observe(section);
+
+var viewportWidth = window.innerWidth;
+animation = document.querySelectorAll(".ani");
+
+if (viewportWidth < 960) {
+  console.log(viewportWidth);
+  animation.forEach(function (animation) {
+    animation.removeAttribute("data-aos");
+    animation.setAttribute("data-aos", "fade-in");
+    console.log(animation.getAttribute("data-aos"));
+  });
+}
